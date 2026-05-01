3f82646e0cf60ab488ca6702a22098214045a4d0
Manu <mandrescabral@gmail.com>
2026-05-01 02:17:15 -0300

    feat: add constants

---
+++ backend/constants.py
@@ -0,0 +1,18 @@
+# Tracker/Stats
+TRACKED_EVENTS = {
+    "UpdateState",
+    "MatchCreated",
+    "MatchInitialized",
+    "MatchDestroyed",
+    "MatchEnded",
+}
+MAX_CONNECT_TRIES = 5
+
+# Discord RPC
+DEFAULT_DISCORD_CLIENT_ID = "1301217342012788826"
+
+# Presence config and presets storage
+PRESENCE_CONFIG_FILE = "presence-config.json"
+PRESETS_FILE = "presence-presets.json"
+PRESETS_VERSION = 1
+MAX_PRESET_NAME_LEN = 64
