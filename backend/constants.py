6df4f41e4d7280e6c950b069ca13f811de7336c4
Manu <mandrescabral@gmail.com>
2026-05-02 04:05:07 -0300

    fix: update STATS_API_WARNING for clearer instructions on configuration

--- backend/constants.py
+++ backend/constants.py
@@ -8,6 +8,18 @@ TRACKED_EVENTS = {
 }
 MAX_CONNECT_TRIES = 5
 
+STATS_API_WARNING = """Before launching Rocket League, edit:
+
+<Install Dir>\\TAGame\\Config\\DefaultStatsAPI.ini
+
+Use at least:
+
+PacketSendRate=30 (any value > 0 enables the exporter)
+Port=49123
+Restart the game after changing the file."""
+TARGET_PORT = 49123
+
+
 # Discord RPC
 DEFAULT_DISCORD_CLIENT_ID = "1301217342012788826"
 
